f19feef6e806e00ab82941758c49064baa51cd15
Abhishek Singh <sabhishek900@gmail.com>
2023-06-17 12:30:44 +0530

    backend fron quiz builder signup and login done

---
+++ models/users.models.js
@@ -0,0 +1,12 @@
+const { verify } = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const UserShchema = mongoose.Schema({
+
+    email: String,
+    password : String
+})
+
+const UserModel = mongoose.model("user", UserShchema)
+
+module.exports = { UserModel };
