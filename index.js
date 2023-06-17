fd6bd674948c0927f5419e1a2bf327cbb822b78c
Abhishek Singh <sabhishek900@gmail.com>
2023-06-17 13:34:20 +0530

    cors addedd

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const dotenv = require("dotenv")
 const { UserRouter } = require("./routes/user.routes")
 const { auth } = require("./middleware/authentication")
 dotenv.config()
+const cors = require('cors')
 
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/users", UserRouter)
 
